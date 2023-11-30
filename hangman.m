2bc7931c1085e1cc276810f3a6163fc97ca52c3e
amazingEPIC <ezlambrych@outlook.com>
2023-11-30 11:43:31 -0500

    Added more stuffs yay

--- hangman.m
+++ hangman.m
@@ -2,47 +2,66 @@ function hangman
     clear
     clc
 
-    test_scene = simpleGameEngineModified('spritesheet.png', 32, 32, 2, [230,230,230], true);
-    background = [getSpriteFromXY(test_scene, 2:11, 8:15)];
-    foreground = ones(8,10);
+    test_scene = simpleGameEngineModified('spritesheet.png', 16, 16, 2, [230,230,230], true);
+    background = [getSpriteFromXY(test_scene, 3:22, 15:30)];
+    size(background)
+    foreground = ones(16,20);
 
-    % Draw title screen
+    % Draw initial title screen
+    foreground = ones(16,20);
 
-    % Draw 
+    foreground(13:14, :) = [getSpriteFromXY(test_scene, 3:22, 37:38)];
+    drawScene(test_scene, background, foreground);
+    getKeyboardInput(test_scene);
+    pause(0.2);
 
-    while(true)
+    foreground(12:15, :) = ones(4,20); % clears bottom bar
+    foreground(12, :) = [getSpriteFromXY(test_s cene, 3:22, 33)];
+    foreground(15, :) = [getSpriteFromXY(test_scene, 3:22, 34)];
+    drawScene(test_scene, background, foreground);
+
+    getKeyboardInput(test_scene);
+    pause(0.2);
+
+    % USEFUL FUNCTIONS I COULD MAKE:
+    % - getLetterFromUser
+    % - generateNewWord
+    % - drawSmallCharacter
+    % - drawLargeCharacter
+    % - drawUnknownWord
+    % - clearBottomBar
     
-        % draw no stick figure
-        foreground = ones(8,10);
-        foreground(3:4,2) = getSpriteFromXY(test_scene, 1, 1);
-        drawScene(test_scene, background, foreground);
-        title("No stick figure (press space to go to next screen)");
+    % USEFUL VARIABLES
+    % - unknownWord
+    % - guessedLetters (character array?)
+    % - guessesUsed
+    % - totalNumberOfGuesses
 
+    % Confirmation screen
+
+    foreground(12:15, :) = ones(4,20); % clears bottom bar
+    foreground(12, :) = [getSpriteFromXY(test_scene, 3:22, 35)];
+    foreground(15, :) = [getSpriteFromXY(test_scene, 3:22, 36)];
+    drawScene(test_scene, background, foreground);
+
+    confirmationInput = '';
+
+    while (~isequal(confirmationInput, 'return') && ~isequal(confirmationInput, 'escape'))
+        confirmationInput = getKeyboardInput(test_scene)
         pause(0.2);
-        getKeyboardInput(test_scene);
     
-        % draw partial stick figure
-        foreground = ones(8,10);
-        foreground(3:4,2) = getSpriteFromXY(test_scene, 5, 4:5);
-        foreground(3,6:8) = getSpriteFromXY(test_scene, 2:4, 1);
-        drawScene(test_scene, background, foreground);
-        title("Partial stick figure");
-
-        pause(0.2);
-        getKeyboardInput(test_scene);
-
-        % draw full stick figure
-        foreground = ones(8,10);
-        foreground(3:4,2) = getSpriteFromXY(test_scene, 9, 4:5);
-        foreground(3,6:9) = getSpriteFromXY(test_scene, 2:5, 1);
-        foreground(4,7:8) = getSpriteFromXY(test_scene, 6:7, 1);
-        drawScene(test_scene, background, foreground);
-        title("Full stick figure");
-
-        pause(0.2);
-        getKeyboardInput(test_scene);
+        if (isequal(confirmationInput, 'return'))
     
-    end
+        elseif (isequal(confirmationInput, 'escape'))
+    
+        end
+
+        fprintf("AAA");
+
+    end   
+    
+
+
 end
 
 
